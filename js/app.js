7ee4e0695f690ab03de33d8a0bb8597e1a2e8f2b
Alexander Semenov <tvaroh@icloud.com>
2014-07-19 20:41:38 +0300

    Update to Morearty 0.1.6.

--- js/app.js
+++ js/app.js
@@ -121,7 +121,7 @@ var TodoList = Ctx.createClass({
 
 var TodoItem = Ctx.createClass({
   componentDidUpdate: function () {
-    if (this.getState().val('editing') !== this.getPreviousState().get('editing')) {
+    if (Ctx.changed(this.getState().sub('editing'))) {
       var node = this.refs.editField.getDOMNode();
       node.focus();
       node.setSelectionRange(node.value.length, node.value.length);
