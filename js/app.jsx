e2d0176e65a463de46b0d551e375da4511311406
Alexander Semenov <tvaroh@icloud.com>
2015-01-22 21:06:48 +0300

    Work-in-progress.

--- js/app.jsx
+++ js/app.jsx
@@ -6,12 +6,9 @@ var Router = require('react-router');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 var RouteHandler = Router.RouteHandler;
+var Link = Router.Link;
 
-var Ctx = Morearty.createContext({
-  options: {
-    renderOnce: true
-  }
-});
+var Ctx = Morearty.createContext({});
 
 var Dashboard = React.createClass({
   mixins: [Morearty.Mixin],
@@ -60,28 +57,77 @@ var Foo = React.createClass({
 var App = React.createClass({
   mixins: [Morearty.Mixin],
   render: function () {
+    console.log('app render');
     return (
       <div>
         <h1>App</h1>
-        <RouteHandler />
+        <header>
+          <ul>
+            <li><Link to="app">Dashboard</Link></li>
+            <li><Link to="inbox">Inbox</Link></li>
+            <li><Link to="calendar">Calendar</Link></li>
+          </ul>
+        </header>
+        <RouteHandler binding={this.getBinding()} />
       </div>
     );
   }
 });
 
+var RouterMoreartyWrapper = React.createClass({
+  contextTypes: {
+    getRouteAtDepth: React.PropTypes.func.isRequired,
+    getRouteComponents: React.PropTypes.func.isRequired,
+    routeHandlers: React.PropTypes.array.isRequired
+  },
+
+  childContextTypes: {
+    makePath: React.PropTypes.func.isRequired,
+    makeHref: React.PropTypes.func.isRequired,
+    transitionTo: React.PropTypes.func.isRequired,
+    replaceWith: React.PropTypes.func.isRequired,
+    goBack: React.PropTypes.func.isRequired,
+    routeHandlers: React.PropTypes.array.isRequired,
+    getCurrentPath: React.PropTypes.func.isRequired,
+    getCurrentRoutes: React.PropTypes.func.isRequired,
+    getCurrentPathname: React.PropTypes.func.isRequired,
+    getCurrentParams: React.PropTypes.func.isRequired,
+    getCurrentQuery: React.PropTypes.func.isRequired,
+    isActive: React.PropTypes.func.isRequired,
+    getRouteAtDepth: React.PropTypes.func.isRequired,
+    getRouteComponents: React.PropTypes.func.isRequired
+  },
+
+  render: function () {
+    console.log('wrapper render');
+    var RouteHandlerWithContext = Ctx.bootstrap(RouteHandler, this.context);
+    return <RouteHandlerWithContext />;
+  }
+});
+
 var routes = (
-  <Route name="app" path="/" handler={Ctx.bootstrap(App)}>
-    <DefaultRoute handler={Dashboard} />
-    <Route name="inbox" handler={Inbox} />
-    <Route name="calendar" handler={Calendar}>
-      <Route name="foo" handler={Foo} />
+  <Route handler={RouterMoreartyWrapper}>
+    <Route name="app" path="/" handler={App}>
+      <DefaultRoute handler={Dashboard} />
+      <Route name="inbox" handler={Inbox} />
+      <Route name="calendar" handler={Calendar}>
+        <Route name="foo" handler={Foo} />
+      </Route>
     </Route>
   </Route>
 );
 
+var b = false;
+var i = 0;
 Router.run(routes, function (Handler) {
-  React.render(<Handler />, document.body);
-  console.log('!!!');
+  if (!b) {
+    React.render(<Handler />, document.body);
+    b = true;
+  } else {
+    console.log('render');
+    Ctx.queueFullUpdate();
+    Ctx.getBinding().set('dummy', i++); // ask to re-render
+  }
 });
 
 module.exports = {
--- js/bootstrap.jsx
+++ js/bootstrap.jsx
@@ -2,9 +2,9 @@ var React = require('react');
 var app = require('./app');
 var App = app.App, Ctx = app.Ctx;
 
-var Bootstrap = React.createFactory(Ctx.bootstrap(App));
+/*var Bootstrap = React.createFactory(Ctx.bootstrap(App));
 
 React.render(
   Bootstrap(),
   document.getElementById('root')
-);
+);*/
